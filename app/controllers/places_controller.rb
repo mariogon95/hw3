78574a39ca93e129416e9125c512df0919246cb6
Mario Gonzalez <mario.gonzalez@kellogg.northwestern.edu>
2023-05-13 19:47:17 +0000

    added Mexico City

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,32 @@
 class PlacesController < ApplicationController
+  
+  def index
+    # find all Places rows
+    @places = Place.all
+    # render places/index view
+  end
+
+  def show
+    # find a Place
+    @places = Place.find_by({"id" =>params["id"]})
+    # render places/show view with details about Place
+  end
+
+  def new
+    # render view with new place form
+    @place = Place.new
+  end
+  
+  def create
+   # start with a new Place
+   @place = Place.new
+   # assign user-entered form data to Place's columns
+   @place["name"] = params["place"]["name"]
+   
+   # save Place row
+   @place.save 
+   # redirect user
+   redirect_to"/places"
+  end
+
 end
