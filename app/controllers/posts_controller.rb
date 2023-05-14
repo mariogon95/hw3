3d213d90bdd49f7f6217f93cea5a6958fdff427e
Mario Gonzalez <mario.gonzalez@kellogg.northwestern.edu>
2023-05-14 00:10:35 +0000

    Fixed posts problem :)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,18 @@ class PostsController < ApplicationController
   
   def new
     @post = Post.new
-    @place = Place.find_by({"id" => params["place_id"]})
-    @post["place_id"] = @place["id"]
+    @post["place_id"] = params["place_id"]
     # render posts/new view with new Post form
-    end
+  end
 
   def create
     # start with a new Contact
     @post = Post.new
 
     # assign user-entered form data to Post's columns
-    @post["action"] = params["post"]["action"]
+    @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
-    @post["date"] = params["post"]["date"]
+    @post["posted_on"] = params["post"]["posted_on"]
     
     # assign relationship between Post and Place
     @post["place_id"] = params["post"]["place_id"]
